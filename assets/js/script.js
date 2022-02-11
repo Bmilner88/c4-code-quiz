1e1cf695a3dd9aa11e434371f63a84e6097a7aa1
Benny623 <bmilner88@gmail.com>
2022-02-11 16:43:55 -0700

    FINALLY WORKIGN SOMEWHAT PROPERLY

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,20 @@ var welcomeEl = document.querySelector('#welcome');
 var highScoresLink = document.querySelector('#high-scores');
 var startBtn = document.querySelector('#start-btn');
 var timerText = document.querySelector('#timer-text')
-var i = -1;
-var timeLeft = 60;
+var i = 0;
+var timeLeft = 70;
+var score = 0
 
 var questionEl = document.createElement('h3');
 var options = document.createElement('ul');
 var option1 = document.createElement('li');
-var option2 = document.createElement('li');
-var option3 = document.createElement('li');
-var option4 = document.createElement('li');
 option1.setAttribute('id', 'a');
+var option2 = document.createElement('li');
 option2.setAttribute('id', 'b');
+var option3 = document.createElement('li');
 option3.setAttribute('id', 'c');
+var option4 = document.createElement('li');
 option4.setAttribute('id', 'd');
-
 
 const questions = [
     {
@@ -68,11 +68,11 @@ const questions = [
     },
     {
         question: "How long is 2112 by Rush?",
-        a: '6:59',
+        a: '20:34',
         b: '19:15',
         c: '8:12',
-        d: '20:34',
-        answer: 'd'
+        d: '6:59',
+        answer: 'a'
     }
 ];
 
@@ -90,92 +90,74 @@ function quizTimer() {
 };
 
 function startQuiz() {
+    event.preventDefault();
     console.log('quiz started!');
+
     // clear html welcome screen
     welcomeEl.textContent = '';
 
-    welcomeEl.appendChild(questionEl);
-    welcomeEl.appendChild(options);
-
-    options.appendChild(option1);
-    options.appendChild(option2);
-    options.appendChild(option3);
-    options.appendChild(option4);
+    option1.addEventListener('click', checkAnswer)
+    option2.addEventListener('click', checkAnswer);
+    option3.addEventListener('click', checkAnswer);
+    option4.addEventListener('click', checkAnswer);
 
     askQuestion();
     quizTimer();
 }
 
 function askQuestion() {
-    i++;
-    console.log(questions.length)
-    if (i < questions.length) {
+    console.log(i)
+    if (timeLeft != 0 && i < questions.length) {
         questionEl.textContent = questions[i].question;
 
         option1.textContent = questions[i].a;
-        option1.addEventListener('click', function(option1){
-            if(option1.target.id === questions[i].answer) {
-                console.log('correct!');
-                askQuestion();
-            }
-            else {
-                console.log('incorrect!');
-                timeLeft -= 10;
-                askQuestion();
-            }
-        });
-
+        
         option2.textContent = questions[i].b;
-        option2.addEventListener('click', function(option2){
-            if(option2.target.id === questions[i].answer) {
-                console.log('correct!');
-                askQuestion();
-            }
-            else {
-                console.log('incorrect!');
-                timeLeft -= 10;
-                askQuestion();
-            }
-        });
-
+        
         option3.textContent = questions[i].c;
-        option3.addEventListener('click', function(option3){
-            if(option3.target.id === questions[i].answer) {
-                console.log('correct!');
-                askQuestion();
-            }
-            else {
-                console.log('incorrect!');
-                timeLeft -= 10;
-                askQuestion();
-            }
-                
-        });
-
+        
         option4.textContent = questions[i].d;
-        option4.addEventListener('click', function(option4){
-            if(option4.target.id.toString() === questions[i].answer) {
-                console.log('correct!');
-                askQuestion();
-            }
-            else {
-                console.log('incorrect!');
-                timeLeft -= 10;
-                askQuestion();
-            }
-        });
-        console.log(i)
-    } 
-    else if (timeLeft === 0) {
-        welcomeEl.textContent = '';
-        console.log('game over')
-        console.log(i)
+        
+        welcomeEl.appendChild(questionEl);
+        welcomeEl.appendChild(options);
+
+        options.appendChild(option1);
+        options.appendChild(option2);
+        options.appendChild(option3);
+        options.appendChild(option4);
+    }
+    else if(i === questions.length) {
+        gameOver();
     }
     else {
-        welcomeEl.textContent = '';
-        console.log('game over')
-        console.log(i)
+        gameOver();
     }
 }
 
+function checkAnswer(option){
+    if(option.target.id === questions[i].answer) {
+        console.log('correct!');
+        i++
+        askQuestion();
+    }
+    else {
+        console.log('incorrect!');
+        timeLeft -= 10;
+        i++
+        askQuestion();
+    };
+};
+
+function gameOver() {
+    option1.removeEventListener('click', checkAnswer)
+    option2.removeEventListener('click', checkAnswer);
+    option3.removeEventListener('click', checkAnswer);
+    option4.removeEventListener('click', checkAnswer);
+    welcomeEl.textContent = '';
+    timerText.textContent = '';
+    score = timeLeft;
+    console.log('game over')
+};
+
 startBtn.addEventListener('click', startQuiz);
+
