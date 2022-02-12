a105daab43f385da39efef5d295a7911ddf8ea69
Benny623 <bmilner88@gmail.com>
2022-02-11 22:07:10 -0700

    begin adding sorting functionality for scores

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -1,11 +1,20 @@
+var welcomeEl = document.querySelector('#welcome');
 var questionEl = document.createElement('h3');
 var scoreList = document.createElement('ul');
 
 var userId = 0;
 var users = [];
 
+function createLi(initials, score) {
+    var temp = document.createElement('li');
+    temp.textContent = initials + ' ' + score;
+    return temp;
+}
+
 function loadScore() {
     var savedUsers = localStorage.getItem('users');
+
+    welcomeEl.appendChild(scoreList);
 
     if(!savedUsers) {
         return false;
@@ -18,6 +27,8 @@ function loadScore() {
     for (i = 0; i < savedUsers.length; i++) {
         userId++;
         users.push(savedUsers[i])
+        scoreList.append(createLi(users[i].initials, users[i].score));
+        users.score.sort((a, b) => a - b);
     };
 };
 
